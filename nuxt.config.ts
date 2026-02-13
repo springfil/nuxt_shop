febaf23e573c944a0e85943864b163c1dfaf2ebb
Timur Kenzhaev <st.kenzhaev@gmail.com>
2026-02-14 01:13:37 +0300

    lint code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  modules: [
-    "@nuxt/scripts",
-    "@nuxt/image",
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-  ],
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    modules: ["@nuxt/scripts", "@nuxt/image", "@nuxt/eslint", "@nuxt/fonts"],
 });
